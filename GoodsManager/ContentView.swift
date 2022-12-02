315a420297d0b45a224795a0bf6fdda2016e2a04
Honoka Suzuki <21cm0122@jec.ac.jp>
2022-12-02 11:09:56 +0900

    create progect

---
+++ GoodsManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoodsManager
+//
+//  Created by cmStudent on 2022/12/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GoodsManager/GoodsManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoodsManagerApp.swift
+//  GoodsManager
+//
+//  Created by cmStudent on 2022/12/02.
+//
+
+import SwiftUI
+
+@main
+struct GoodsManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
